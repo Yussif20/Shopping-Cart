264535d9714e00cbf632ba875980590fe4dc4df8
Yousef <yousefayman2000500@gmail.com>
2024-03-28 01:21:26 +0200

    migrating the entire project to use the context api

--- src/App.jsx
+++ src/App.jsx
@@ -69,14 +69,12 @@ function App() {
   const ctxValue = {
     items: shoppingCart.items,
     addItemsToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
   };
 
   return (
     <CartContext.Provider value={ctxValue}>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
+      <Header />
       <Shop onAddItemToCart={handleAddItemToCart} />
     </CartContext.Provider>
   );
